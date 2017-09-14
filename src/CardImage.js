ccfa97d729653f40eb68057af9efd87eac37c5c3
alickzhang <alick369@gmail.com>
2017-09-15 00:34:43 +1000

    implement card structure and animation

--- src/CardImage.js
+++ src/CardImage.js
@@ -1,21 +1,47 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
+import './CardImage.css'
 
-class CardImage extends Component {
+const CardImage = (props) => {
+  const { animation, width, src, title, description, shadow } = props
+  let cardClassName = classNames({
+    'card-container': true,
+    shadow: shadow
+  })
+  let imgClassName = classNames(`img-${animation}`)
 
-  static propTypes = {
+  return (
+    <div className={cardClassName} style={{width: `${width}px`}}>
+      <div className="img-container">
+        <div className={imgClassName}>
+          <img src={src} alt={title} />
+        </div>
+      </div>
+      <div className="text-container">
+        <h3 className="title">{title}</h3>
+        <p className="description">{description}</p>
+      </div>
+    </div>
+  )
+}
 
-  };
+CardImage.propTypes = {
+  animation: PropTypes.string,
+  width: PropTypes.number,
+  src: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  shadow: PropTypes.bool
+}
 
-  static defaultProps = {
-
-  };
-
-  render() {
-    return (
-      <div></div>
-    )
-  }
+CardImage.defaultProps = {
+  animation: '',
+  width: 200,
+  src: '',
+  title: '',
+  description: '',
+  shadow: false
 }
 
 export default CardImage
--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom'
 import CardImage from './CardImage'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<CardImage />, document.getElementById('root'))
+const src = 'http://res.cloudinary.com/alick/image/upload/v1502375978/bedtime_stories_bywggz.jpg'
+
+const App = () => {
+  return (
+    <CardImage animation='rotate' src={src} width={200} shadow title='Bedtime Stories' description='Jay Chou Music Jay Chou Music' />
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
